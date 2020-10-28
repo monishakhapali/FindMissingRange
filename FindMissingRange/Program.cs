d533bacbf4e18abfe25d90cd7d862b53491f5192
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-10-28 19:49:48 -0400

    Find the missing range

---
+++ FindMissingRange/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindMissingRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] a = { 1, 2, 3, 50, 52, 75 };
+            int i = 0;
+            while (i < a.Length)
+            {
+                if(i== a.Length-1)
+                {
+                    Console.WriteLine("range is" + (a[i] + 1) + 99);
+                    break;
+                }
+                if (a[i + 1] - a[i] != 1)
+                {
+                    Console.WriteLine("range is" + (a[i] + 1) + " " + (a[i + 1] - 1));
+                }
+                i++;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
